d1c1648ac338e3bfb1661f05c48db6bf06b5f60b
Ferris Boran <ferris.boran@gmail.com>
2019-03-14 17:13:00 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,8 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |name|
+    if name["status"] == "Winner"
+      return name["name"]
+  end
 end
 
 def get_contestant_name(data, occupation)
